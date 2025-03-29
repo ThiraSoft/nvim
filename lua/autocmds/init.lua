8c8a3fb1560a0293ed4ff66a4d4f6dcd9f8a25a1
Ricardo Carmona <ricardo.carmona@hotmail.fr>
2025-03-29 08:33:17 +0100

    Add dashboard screen

--- lua/autocmds/init.lua
+++ lua/autocmds/init.lua
@@ -3,18 +3,18 @@ local normal_hl = vim.api.nvim_get_hl(0, { name = "Normal", link = false })
 Base_bg_color = normal_hl and normal_hl.bg or nil
 
 vim.api.nvim_create_autocmd("BufEnter", {
-  callback = function()
-    local filepath = vim.fn.expand "%:p" -- Récupère le chemin complet du fichier
-    if filepath:match "node_modules" then
-      vim.cmd "hi Normal guibg=#202000" -- Fond jaune
-      vim.bo.modifiable = false -- Rend le buffer non modifiable
-      vim.bo.readonly = true -- Rend le fichier en lecture seule
-    else
-      vim.api.nvim_set_hl(0, "Normal", { bg = Base_bg_color })
-      vim.bo.modifiable = true -- Réactive la modification pour les autres fichiers
-      vim.bo.readonly = false
-    end
-  end,
+	callback = function()
+		local filepath = vim.fn.expand("%:p") -- Récupère le chemin complet du fichier
+		if filepath:match("node_modules") then
+			vim.cmd("hi Normal guibg=#202000") -- Fond jaune
+			vim.bo.modifiable = false -- Rend le buffer non modifiable
+			vim.bo.readonly = true -- Rend le fichier en lecture seule
+		else
+			vim.api.nvim_set_hl(0, "Normal", { bg = Base_bg_color })
+			vim.bo.modifiable = true -- Réactive la modification pour les autres fichiers
+			vim.bo.readonly = false
+		end
+	end,
 })
 
 -- Crée un groupe d'autocommandes pour une gestion propre
@@ -35,51 +35,30 @@ local group = vim.api.nvim_create_augroup("AutoSaveOnFocusLost", { clear = true 
 
 -- Auto save (uniquement si hors node_modules)
 vim.api.nvim_create_autocmd("BufLeave", {
-  group = group,
-  pattern = "*",
-  callback = function()
-    local bufname = vim.api.nvim_buf_get_name(0)
-    if not string.match(bufname, "node_modules") then
-      vim.cmd "silent! w"
-    end
-  end,
+	group = group,
+	pattern = "*",
+	callback = function()
+		local bufname = vim.api.nvim_buf_get_name(0)
+		if not string.match(bufname, "node_modules") then
+			vim.cmd("silent! w")
+		end
+	end,
 })
 
--- Ouverture de nvimtree:
--- On veut le focus auto sur le fichier
--- On les numero de lignes relatifs pour faciliter les sauts de lignes
-local function open_nvim_tree_and_refocus()
-  require("nvim-tree.api").tree.open()
-
-  if vim.fn.argc() ~= 0 then
-    -- Vérifie si un buffer normal existe et reprend le focus dessus
-    local buffers = vim.api.nvim_list_bufs()
-    for _, buf in ipairs(buffers) do
-      vim.opt.number = true
-      vim.opt.relativenumber = true
-      if vim.bo[buf].buftype == "" then
-        vim.api.nvim_set_current_buf(buf)
-        vim.cmd "normal! m-" -- Evite d'ajouter un jump dans la jumplist pour nvimtree
-        return
-      end
-    end
-  end
-end
-
+-- Hook d'ouverture de neovim
 vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    if vim.fn.argc() == 0 then
-      vim.schedule(open_nvim_tree_and_refocus)
-    end
-    vim.cmd "clearjumps" -- Efface la jumplist à chaque ouverture de session
-  end,
+	callback = function()
+		-- Efface la jumplist à chaque ouverture de session
+		vim.cmd("clearjumps")
+	end,
 })
 
+-- Numeros de lignes dans nvimtree
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "NvimTree",
-  callback = function()
-    vim.defer_fn(function()
-      vim.wo.relativenumber = true -- Active les numéros relatifs
-    end, 100) -- Attend 100ms pour s'assurer que la fenêtre est prête
-  end,
+	pattern = "NvimTree",
+	callback = function()
+		vim.defer_fn(function()
+			vim.wo.relativenumber = true -- Active les numéros relatifs
+		end, 100) -- Attend 100ms pour s'assurer que la fenêtre est prête
+	end,
 })
--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,37 +6,110 @@
 local M = {}
 
 M.base46 = {
-  theme = "custom",
-  transparency = false,
+	theme = "custom",
+	theme_toggle = { "custom", "github_light" },
+	transparency = false,
 
-  -- hl_override = {
-  -- 	Comment = { italic = true },
-  -- 	["@comment"] = { italic = true },
-  -- },
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
 }
 
-vim.cmd "highlight St_relativepath guifg=#626a83 guibg=#2a2b36"
+vim.cmd("highlight St_relativepath guifg=#626a83 guibg=#2a2b36")
 
 local stbufnr = function()
-  return vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
+	return vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
 end
 
 M.ui = {
-  statusline = {
-    theme = "default",
-    order = { "mode", "relativepath", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd" },
-    -- order = { "mode", "relativepath", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
-    modules = {
-      relativepath = function()
-        local path = vim.api.nvim_buf_get_name(stbufnr())
 
-        if path == "" then
-          return ""
-        end
+	telescope = { style = "borderless" }, -- borderless / bordered
+	statusline = {
+		theme = "default",
+		order = { "mode", "relativepath", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd" },
+		-- order = { "mode", "relativepath", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
+		modules = {
+			relativepath = function()
+				local path = vim.api.nvim_buf_get_name(stbufnr())
 
-        return "%#St_relativepath#  " .. vim.fn.expand "%:.:h" .. " /"
-      end,
-    },
-  },
+				if path == "" then
+					return ""
+				end
+
+				return "%#St_relativepath#  " .. vim.fn.expand("%:.:h") .. " /"
+			end,
+		},
+	},
+}
+
+M.nvdash = {
+	load_on_startup = true,
+	-- header = {
+	-- 	"                           ",
+	-- 	"                           ",
+	-- 	"                           ",
+	-- 	"           eovim          ",
+	-- 	"                           ",
+	-- 	"                           ",
+	-- 	"                           ",
+	-- },
+
+	header = {
+
+		"                                                                   ",
+		"                                                                   ",
+		"                                                                   ",
+		"    .                                                         .    ",
+		"     .                                                       .     ",
+		"      .                                                     .      ",
+		"       ...                                               ...       ",
+		"         ....                                         ....         ",
+		"     .     . ...                                   ... .     .     ",
+		"      ..        ..                               ..        ..      ",
+		"      .....        ..                         ..        .....      ",
+		"       ........      ..                      .      ........       ",
+		"         ..........    .                   .    ..........         ",
+		"             .........   .               .   .........             ",
+		"        ................. .             . .................        ",
+		"            ...............             ...............            ",
+		"              ..............           ..............              ",
+		"             .   ...  ...  o   eovim  o  ...  ...   .             ",
+		"                 .  .      .           .      .  .                 ",
+		"                   .    ...             ...    .                   ",
+		"                      ....               ....                      ",
+		"                    .....                 .....                    ",
+		"                  ......                   ......                  ",
+		"                 ......                     ......                 ",
+		"                .......                     .......                ",
+		"                ..... .                     . .....                ",
+		"               . ....  .                      .... .               ",
+		"               ..  ..                         ..  ..               ",
+		"                    .                         .                    ",
+		"              .                                     .              ",
+		"              .                                     .              ",
+		"                                                                   ",
+	},
+
+	buttons = {
+		{ txt = "", cmd = "Telescope oldfiles" },
+		-- { txt = "  Recent Files", cmd = "Telescope oldfiles" },
+		-- { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
+		-- { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
+
+		-- { txt = "─", hl = "NvDashFooter", no_gap = false, rep = true },
+		--
+		-- {
+		-- 	txt = function()
+		-- 		local stats = require("lazy").stats()
+		-- 		local ms = math.floor(stats.startuptime) .. " ms"
+		-- 		return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+		-- 	end,
+		-- 	hl = "NvDashFooter",
+		-- 	no_gap = true,
+		-- },
+		--
+		-- { txt = "─", hl = "NvDashFooter", no_gap = false, rep = true },
+	},
 }
 return M
