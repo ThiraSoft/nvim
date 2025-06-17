036dacd46edde64638102b6d5e350d026b94c003
Ricardo Carmona <ricardo.carmona@cbainfo.fr>
2025-06-17 22:53:30 +0200

    add diff

--- lua/autocmds/init.lua
+++ lua/autocmds/init.lua
@@ -3,18 +3,18 @@ local normal_hl = vim.api.nvim_get_hl(0, { name = "Normal", link = false })
 Base_bg_color = normal_hl and normal_hl.bg or nil
 
 vim.api.nvim_create_autocmd("BufEnter", {
-	callback = function()
-		local filepath = vim.fn.expand("%:p") -- Récupère le chemin complet du fichier
-		if filepath:match("node_modules") then
-			vim.cmd("hi Normal guibg=#202000") -- Fond jaune
-			vim.bo.modifiable = false -- Rend le buffer non modifiable
-			vim.bo.readonly = true -- Rend le fichier en lecture seule
-		else
-			vim.api.nvim_set_hl(0, "Normal", { bg = Base_bg_color })
-			vim.bo.modifiable = true -- Réactive la modification pour les autres fichiers
-			vim.bo.readonly = false
-		end
-	end,
+  callback = function()
+    local filepath = vim.fn.expand "%:p" -- Récupère le chemin complet du fichier
+    if filepath:match "node_modules" then
+      vim.cmd "hi Normal guibg=#202000" -- Fond jaune
+      vim.bo.modifiable = false -- Rend le buffer non modifiable
+      vim.bo.readonly = true -- Rend le fichier en lecture seule
+    else
+      vim.api.nvim_set_hl(0, "Normal", { bg = Base_bg_color })
+      vim.bo.modifiable = true -- Réactive la modification pour les autres fichiers
+      vim.bo.readonly = false
+    end
+  end,
 })
 
 -- Crée un groupe d'autocommandes pour une gestion propre
@@ -35,30 +35,61 @@ local group = vim.api.nvim_create_augroup("AutoSaveOnFocusLost", { clear = true 
 
 -- Auto save (uniquement si hors node_modules)
 vim.api.nvim_create_autocmd("BufLeave", {
-	group = group,
-	pattern = "*",
-	callback = function()
-		local bufname = vim.api.nvim_buf_get_name(0)
-		if not string.match(bufname, "node_modules") then
-			vim.cmd("silent! w")
-		end
-	end,
+  group = group,
+  pattern = "*",
+  callback = function()
+    local bufname = vim.api.nvim_buf_get_name(0)
+    if not string.match(bufname, "node_modules") then
+      vim.cmd "silent! w"
+    end
+  end,
 })
 
 -- Hook d'ouverture de neovim
 vim.api.nvim_create_autocmd("VimEnter", {
-	callback = function()
-		-- Efface la jumplist à chaque ouverture de session
-		vim.cmd("clearjumps")
-	end,
+  callback = function()
+    -- Efface la jumplist à chaque ouverture de session
+    vim.cmd "clearjumps"
+  end,
 })
 
 -- Numeros de lignes dans nvimtree
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "NvimTree",
-	callback = function()
-		vim.defer_fn(function()
-			vim.wo.relativenumber = true -- Active les numéros relatifs
-		end, 100) -- Attend 100ms pour s'assurer que la fenêtre est prête
-	end,
+  pattern = "NvimTree",
+  callback = function()
+    vim.defer_fn(function()
+      vim.wo.relativenumber = true -- Active les numéros relatifs
+    end, 100) -- Attend 100ms pour s'assurer que la fenêtre est prête
+  end,
+})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    -- Un petit délai pour laisser NvChad finir de charger tous ses highlights
+    vim.defer_fn(function()
+      local colors = {
+        diff_add = "#50FA7B",
+        diff_change = "#F1FA8C",
+        diff_delete = "#FF5555",
+        diff_text = "#BD93F9",
+        diff_blend = 0, -- Assure que le fond est plein
+      }
+      vim.api.nvim_set_hl(0, "DiffAdd", { fg = "NONE", bg = colors.diff_add, ctermbg = 155, blend = colors.diff_blend })
+      vim.api.nvim_set_hl(
+        0,
+        "DiffChange",
+        { fg = "NONE", bg = colors.diff_change, ctermbg = 227, blend = colors.diff_blend }
+      )
+      vim.api.nvim_set_hl(
+        0,
+        "DiffDelete",
+        { fg = "NONE", bg = colors.diff_delete, ctermbg = 196, blend = colors.diff_blend }
+      )
+      vim.api.nvim_set_hl(
+        0,
+        "DiffText",
+        { fg = "NONE", bg = colors.diff_text, ctermbg = 165, blend = colors.diff_blend }
+      )
+    end, 100) -- Délai de 100ms
+  end,
 })
--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -2,62 +2,62 @@
 local M = {}
 
 M.base46 = {
-	theme = "custom",
-	theme_toggle = { "custom", "github_light" },
-	transparency = false,
+  theme = "custom",
+  theme_toggle = { "custom", "custom" },
+  transparency = false,
 }
 
-vim.cmd("highlight St_relativepath guifg=#626a83 guibg=#2a2b36")
+vim.cmd "highlight St_relativepath guifg=#626a83 guibg=#2a2b36"
 
 local stbufnr = function()
-	return vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
+  return vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
 end
 
 M.ui = {
-	telescope = { style = "borderless" }, -- borderless / bordered
-	statusline = {
-		theme = "default",
-		order = { "mode", "relativepath", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd" },
-		-- order = { "mode", "relativepath", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
-		modules = {
-			relativepath = function()
-				local path = vim.api.nvim_buf_get_name(stbufnr())
+  telescope = { style = "borderless" }, -- borderless / bordered
+  statusline = {
+    theme = "default",
+    order = { "mode", "relativepath", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd" },
+    -- order = { "mode", "relativepath", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
+    modules = {
+      relativepath = function()
+        local path = vim.api.nvim_buf_get_name(stbufnr())
 
-				if path == "" then
-					return ""
-				end
+        if path == "" then
+          return ""
+        end
 
-				return "%#St_relativepath#  " .. vim.fn.expand("%:.:h") .. " /"
-			end,
-		},
-	},
+        return "%#St_relativepath#  " .. vim.fn.expand "%:.:h" .. " /"
+      end,
+    },
+  },
 }
 
 local logo = require("logos").get_random()
 
 M.nvdash = {
-	load_on_startup = true,
-	header = logo,
+  load_on_startup = true,
+  header = logo,
 
-	buttons = {
-		{ txt = "  Recent Files", cmd = "Telescope oldfiles" },
-		{ txt = "  New File", cmd = "enew" },
-		-- { txt = "  Find File", cmd = "Telescope find_files" },
-		-- { txt = "󰈭  Find Word", cmd = "Telescope live_grep" },
+  buttons = {
+    { txt = "  Recent Files", cmd = "Telescope oldfiles" },
+    { txt = "  New File", cmd = "enew" },
+    -- { txt = "  Find File", cmd = "Telescope find_files" },
+    -- { txt = "󰈭  Find Word", cmd = "Telescope live_grep" },
 
-		-- { txt = "─", hl = "NvDashFooter", no_gap = false, rep = true },
-		--
-		-- {
-		-- 	txt = function()
-		-- 		local stats = require("lazy").stats()
-		-- 		local ms = math.floor(stats.startuptime) .. " ms"
-		-- 		return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
-		-- 	end,
-		-- 	hl = "NvDashFooter",
-		-- 	no_gap = true,
-		-- },
-		--
-		-- { txt = "─", hl = "NvDashFooter", no_gap = false, rep = true },
-	},
+    -- { txt = "─", hl = "NvDashFooter", no_gap = false, rep = true },
+    --
+    -- {
+    -- 	txt = function()
+    -- 		local stats = require("lazy").stats()
+    -- 		local ms = math.floor(stats.startuptime) .. " ms"
+    -- 		return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+    -- 	end,
+    -- 	hl = "NvDashFooter",
+    -- 	no_gap = true,
+    -- },
+    --
+    -- { txt = "─", hl = "NvDashFooter", no_gap = false, rep = true },
+  },
 }
 return M
